bc597ed7903704ce68442147184008b159ade7ce
madhums0411 <madhums41175@gmail.com>
2022-07-24 18:59:50 +0530

    Player reach winning position

--- SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder/SnakeAndLadder.cs
@@ -12,34 +12,46 @@ namespace SnakeAndLadderProblem
         const int NO_PLAY = 1;
         const int LADEER = 2;
         const int SNAKE = 3;
+        const int WINNING_POSITION = 100;
         public void StartGame()
         {
             Console.WriteLine("Game Started ");
             Console.WriteLine("Player Position:" + playerPosition);
-            Random random = new Random();
-            int dieRolled = random.Next(1, 7);
-            Console.WriteLine("Die Rolled: " + dieRolled);
-            playerPosition += dieRolled;
-            Console.WriteLine("Player position:" + playerPosition);
-            int option = random.Next(1, 4);
 
-            switch (option)
+            while (playerPosition < WINNING_POSITION)
             {
-                case NO_PLAY:
-                    Console.WriteLine("No play");
-                    break;
+                Random random = new Random();
+                int dieRolled = random.Next(1, 7);
+                int option = random.Next(1, 4);
 
-                case LADEER:
-                    playerPosition += dieRolled;
-                    Console.WriteLine("Ladder: " + playerPosition);
-                    break;
+                switch (option)
+                {
+                    case NO_PLAY:
+                        Console.WriteLine("No play");
+                        break;
 
-                case SNAKE:
-                    playerPosition -= dieRolled;
-                    Console.WriteLine("Snake: " + playerPosition);
-                    break;
+                    case LADEER:
+                        playerPosition += dieRolled;
+                        Console.WriteLine(" Dice Rolls Number : +{0} ", dieRolled);
+                        Console.WriteLine("Ladder: " + playerPosition);
+                        break;
 
+                    case SNAKE:
+                        playerPosition -= dieRolled;
+                        Console.WriteLine(" Dice Rolls Number : -{0} ", dieRolled);
+                        Console.WriteLine("Snake: " + playerPosition);
+                        if (playerPosition < 0)
+                        {
+                            playerPosition = 0;
+                            Console.WriteLine("Player restart from :" + playerPosition);
+                        }
+                        break;
 
+                }
+                if (playerPosition >= WINNING_POSITION)
+                {
+                    Console.WriteLine("Player Won the Game");
+                }
             }
         }
     }
